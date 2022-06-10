02fcf6f81310bdb51174ac2af748d32a47d28134
pj perry <pjperry28@gmail.com>
2022-06-10 10:34:54 -0700

    stuck on where to start business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 
-let array = []
- let num = $().val
+let stringArray = []
+ let userNumber = $("input#number").val();
+ for(index = 0, index <= user , index++) {
+  if (index.toString().includes (3)) {
+    stringHolder.push("won't you be my neighbor")
+    }else if(index.toString().includes(2)) {
+      stringHolder.push("*boop*")
+    }else if(index.toString().includes(1)) {
+      stringHolder.push("*beep*")
+    }else {
+      stringHolder.push(index);
+    }
+  }
+  return stringArray
+
+  
+
+
+
  
+
+
+
