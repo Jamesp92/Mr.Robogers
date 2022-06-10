6124775028831cae3bb28ad0f7bc7d4febe164d4
pj perry <pjperry28@gmail.com>
2022-06-10 16:45:01 -0700

    rewrite javascript / update README

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,38 @@
 //business logic
 
 function inputNumber(userInput){
-   let outputArray = []
-    for (let index = 0; index <= userInput; index++) {
-       outputArray.push(index .toString().split(","));
-    }
-       
-        return outputArray;
-  }
-  function phraseNumbers (userInput){
-   let respondingArray = [];
-   const outputArray = inputNumber(userInput);
-    respondingArray.forEach(function (phrase) {
-        if (phrase.includes(3)) {
-          phrase = "Won't you be my neighbor?";
-          respondingArray.push(phrase);
-      } else if (phrase.includes(2)) {
-          phrase = "*Boop*";
-          respondingArray.push(phrase);
-       } else if (phrase.includes(1)) {
-          phrase = "*Beep*";
-          respondingArray.push(phrase);
-      } else {
-          respondingArray.push(phrase);
-       }
-  });
+   let inputArray = []
+   for (let i = 0; i <= userInput; i++) {
+      inputArray.push( i.toString().split(","));
+   }
+   return inputArray;
+}
+      let respondingArray = [];
+      respondingArray.forEach(function (phrase) {
+   if (phrase.includes(3)) {
+        phrase = "Won't you be my neighbor?";
+        respondingArray.push(phrase);
+    } else if (phrase.includes(2)) {
+        phrase = "*Boop*";
+        respondingArray.push(phrase);
+     } else if (phrase.includes(1)) {
+        phrase = "*Beep*";
+        respondingArray.push(phrase);
+    } else {
+        respondingArray.push(phrase);
+     }
+});
 return respondingArray;
 }
+//   function phraseNumbers (userInput){
+   // const inputArray = inputNumber(userInput);
 
 $(document).ready(function () {
   $("form#userInput").submit(function (event) {
     event.preventDefault();
-    const results = result (userInput);
-    $("#output").text(results);
+    let response = inputNumber(userInput);
+    $("#output").text(inputArray)
+  
+
   });
 });
