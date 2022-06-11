1b2b19b96aeb34a559f4a77459f3ab385d63cc49
pj perry <pjperry28@gmail.com>
2022-06-11 12:26:44 -0700

    ui logic is working input now displaying on page

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,28 @@
 function userInputNumbers(userNum) {
    let numbers = []
    for (let i = 0; i <= userNum; i ++) {
-     numbers.push(i)
-     if (numbers[i].toString().includes(3)) {
-       numbers[i] = "Won't you be my neighbor?"
-     } else if (numbers[i].toString().includes(2)) {
-       numbers[i] = "*boop*"
-     } else if (numbers[i].toString().includes(1)) {
-       numbers[i] = "*beep*"
-     }
+      numbers.push(i)
+      if (numbers[i].toString().includes(3)) {
+         numbers[i] = "Won't you be my neighbor?"
+      } else if (numbers[i].toString().includes(2)) {
+         numbers[i] = "*boop*"
+      } else if (numbers[i].toString().includes(1)) {
+         numbers[i] = "*beep*"
+      }
    }  
    return numbers
- }
+}
 
- $(document).ready(function() {
-   $("form#userNum").submit(function (event) {
-      event.preventDefault(); 
-     let result = userInputNumbers ($("userNum").val());
-      $("#output").text(result);
-      $("#output").show()
-      
 
-   });
+
+$(document).ready(function() {
+  $("#form").submit(function (event) {
+     event.preventDefault(); 
+     const userNumber = $("#userNum").val();
+     let result = userInputNumbers(userNumber);
+     $("#output").text(result);
+     $("#output").show() 
+});
 });
 
 
