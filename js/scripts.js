cb08a47de4a6e542da152c4b797894b2db041304
pj perry <pjperry28@gmail.com>
2022-06-10 17:19:48 -0700

    create initial function and start else if

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,11 @@
-//business logic
-
-function inputNumber(userInput){
-   let inputArray = []
-   for (let i = 0; i <= userInput; i++) {
-      inputArray.push( i.toString().split(","));
-   }
-   return inputArray;
+ function userInputNumbers (input)
+ let numbers =[]
+ for (let i = 0; i  <= userInput; 1++){
+     numbers.push(1)
+   if (numbers[i].toString().includes(3))
+    numbers[i] = "wont you be my neighbor?"
+} else if (numbers[i].toString().includes(2)){
+   numbers[i] = "*boop*"
 }
-      let respondingArray = [];
-      respondingArray.forEach(function (phrase) {
-   if (phrase.includes(3)) {
-        phrase = "Won't you be my neighbor?";
-        respondingArray.push(phrase);
-    } else if (phrase.includes(2)) {
-        phrase = "*Boop*";
-        respondingArray.push(phrase);
-     } else if (phrase.includes(1)) {
-        phrase = "*Beep*";
-        respondingArray.push(phrase);
-    } else {
-        respondingArray.push(phrase);
-     }
-});
-return respondingArray;
-}
-//   function phraseNumbers (userInput){
-   // const inputArray = inputNumber(userInput);
 
-$(document).ready(function () {
-  $("form#userInput").submit(function (event) {
-    event.preventDefault();
-    let response = inputNumber(userInput);
-    $("#output").text(inputArray)
-  
 
-  });
-});
